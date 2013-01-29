71b29167ac4dc2756965b4a08a504e4c6655121e
Martin Keegan <martin@no.ucant.org>
2013-01-29 04:34:42 +0000

    allow multiple key columns; catch duplicates

--- src/decant.py
+++ src/decant.py
@@ -6,6 +6,7 @@
 #  it under the terms of the Apache Software Licence v2.0
 
 import oursql
+import sys
 
 def _transfer_entries(upstream, downstream, 
                      src_tbl, dst_tbl, 
@@ -14,8 +15,14 @@ def _transfer_entries(upstream, downstream,
 
     def decant():
         assert len(src_cols) == len(dst_cols)
-        assert src_key_col in src_cols
-        assert dst_key_col in dst_cols
+        for col in src_key_col:
+            if col not in src_key_col:
+                print "Source key column '%s' not mentioned in mapping" % col
+                sys.exit(1)
+        for col in dst_key_col:
+            if col not in dst_key_col:
+                print "Destination key column '%s' not mentioned in mapping" % col
+                sys.exit(1)
 
         src_fields = ", ".join(src_cols)
         dst_fields = ", ".join(dst_cols)
@@ -28,30 +35,34 @@ def _transfer_entries(upstream, downstream,
             rows = c.fetchall()
 
         # get the key column from downstream
-        sql = '''select %s from %s''' % (dst_key_col, dst_tbl)
+        dst_key_col_names = ", ".join(dst_key_col)
+        sql = '''select %s from %s''' % (dst_key_col_names, dst_tbl)
 
         c = downstream.cursor()
         c.execute(sql)
-        dst_keys = [ i[0] for i in c.fetchall() ]
+        dst_keys = [ i for i in c.fetchall() ]
 
         # remove rows which are already present downstream
-        new_rows = filter(lambda x: x[src_key_col] not in dst_keys, rows)
+        def slice(row, columns):
+            return tuple([ row[c] for c in columns ])
+        new_rows = filter(lambda x: slice(x, src_key_col) not in dst_keys, rows)
 
         dst_placeholders = ", ".join(["%s"] * len(dst_cols))
         sql = '''insert into %s (%s) values (%s);''' % (dst_tbl, 
                                                         dst_fields, 
                                                         dst_placeholders)
-        lookup = dict(zip(src_cols, dst_cols))
+        lookup = dict(zip(dst_cols, src_cols))
 
-        #print "LOOKUP", lookup
-        if len(new_rows) > 1:
-            pass # print "NEW_ROWS[0]", new_rows[0]
-        #print "SRC_COLS", src_cols
-
+        unique_keys = {}
         # insert remaining rows into downstream
         for row in new_rows:
-            #print "ROW", row
             dst_values = map(lambda i: row[i], src_cols)
+
+            key = slice(row, [ lookup[dkc] for dkc in dst_key_col ])
+            if key in unique_keys:
+                continue # aaaargh
+            unique_keys[key] = True
+
             c = downstream.cursor()
             rv = c.execute(sql, dst_values)
         downstream.commit()
